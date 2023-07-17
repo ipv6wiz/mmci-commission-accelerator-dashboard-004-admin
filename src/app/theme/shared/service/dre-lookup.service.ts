489fc51fd311af6896bb9f7aab56df43c80b0e5f
ipv6wiz <john@thejohnpage.com>
2023-07-16 21:19:31 -0700

    Added comment to DRE lookup

--- src/app/theme/shared/service/dre-lookup.service.ts
+++ src/app/theme/shared/service/dre-lookup.service.ts
@@ -7,6 +7,10 @@ export class DreLookupService {
     private verifyDreLicenseUrl = 'http://127.0.0.1:5001/comm-acc-demo/us-central1/verifyDreLicense';
     constructor(private http: HttpClient) {}
 
+    /**
+     * This will ultimately be updated to use the API
+     * @param dreNumber
+     */
     checkLicense(dreNumber: string): Observable<any> {
         return this.http.get(`${this.verifyDreLicenseUrl}?dre=${dreNumber}`);
     }
