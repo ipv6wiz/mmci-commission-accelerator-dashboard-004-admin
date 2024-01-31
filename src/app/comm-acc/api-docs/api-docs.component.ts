740907f7e67e4e7959f0c73edec904e62d6d98a5
ipv6wiz <john@thejohnpage.com>
2024-01-30 22:46:29 -0800

    Deployed Version 0.0.44. hunting require not defined bug - commented out Swagger

--- src/app/comm-acc/api-docs/api-docs.component.ts
+++ src/app/comm-acc/api-docs/api-docs.component.ts
@@ -1,7 +1,8 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {SharedModule} from "../../theme/shared/shared.module";
-import SwaggerUI,  {SwaggerUIOptions } from 'swagger-ui';
+// import SwaggerUI,  {SwaggerUIOptions } from 'swagger-ui';
+
 // import { SwaggerConfigs, SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
 import {environment} from "../../../environments/environment";
 @Component({
@@ -19,37 +20,35 @@ export default class ApiDocsComponent implements  AfterViewInit {
     constructor() {}
 
     ngAfterViewInit() {
-        const swagConfig: SwaggerUIOptions = {
-            url: `${this.apiUrl}api-docs-json`,
-            requestInterceptor: (request: any) => {
-                console.log('request:'+JSON.stringify(request));
-                const addAuth = (request['url'].indexOf('/login') === -1);
-                const user = sessionStorage.getItem('user');
-                console.log('requestInterceptor - user: ', user);
-                if(user) {
-                    const currentUser = JSON.parse(user);
-                    if (currentUser && currentUser.idToken && addAuth) {
-                        request['headers']['Authorization'] = 'Bearer ' + currentUser.idToken;
-                    } else {
-                        console.log("No sessionStorage");
-                    }
-                    console.log('request-modified:'+JSON.stringify(request));
-                    request['headers']['Cache-Control'] = 'no-store no-cache';
-                }
-                return request;
-            },
-            responseInterceptor: (response: any) => {
-                console.log('responseInterceptor - response: ', response);
-                return response;
-            },
-            dom_id: 'swagger',
-            domNode: this.swaggerDom.nativeElement,
-            deepLinking: true,
-            // presets: [SwaggerUIBundle['presets'].apis, SwaggerUIStandalonePreset],
-            // layout: 'StandaloneLayout'
-        };
-        console.log('ApiDocsComponent - ngAfterViewInit - swagConfig: ', JSON.stringify(swagConfig));
-        SwaggerUI(swagConfig);
-        // SwaggerUIBundle(swagConfig)
+        console.log('So this is not lonely!!!')
+        // const swagConfig: SwaggerUIOptions = {
+        //     url: `${this.apiUrl}api-docs-json`,
+        //     requestInterceptor: (request: any) => {
+        //         console.log('request:'+JSON.stringify(request));
+        //         const addAuth = (request['url'].indexOf('/login') === -1);
+        //         const user = sessionStorage.getItem('user');
+        //         console.log('requestInterceptor - user: ', user);
+        //         if(user) {
+        //             const currentUser = JSON.parse(user);
+        //             if (currentUser && currentUser.idToken && addAuth) {
+        //                 request['headers']['Authorization'] = 'Bearer ' + currentUser.idToken;
+        //             } else {
+        //                 console.log("No sessionStorage");
+        //             }
+        //             console.log('request-modified:'+JSON.stringify(request));
+        //             request['headers']['Cache-Control'] = 'no-store no-cache';
+        //         }
+        //         return request;
+        //     },
+        //     responseInterceptor: (response: any) => {
+        //         console.log('responseInterceptor - response: ', response);
+        //         return response;
+        //     },
+        //     dom_id: 'swagger',
+        //     domNode: this.swaggerDom.nativeElement,
+        //     deepLinking: true,
+        // };
+        // console.log('ApiDocsComponent - ngAfterViewInit - swagConfig: ', JSON.stringify(swagConfig));
+        // SwaggerUI(swagConfig);
     }
 }
