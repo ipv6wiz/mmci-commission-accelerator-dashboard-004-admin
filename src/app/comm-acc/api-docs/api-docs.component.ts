6b814477fe7e3a7e963aad240c34364ff21574ab
ipv6wiz <john@thejohnpage.com>
2024-01-30 21:56:05 -0800

    Deployed Version 0.0.42. hunting require not defined bug - trying a different approach for the Api Docs

--- src/app/comm-acc/api-docs/api-docs.component.ts
+++ src/app/comm-acc/api-docs/api-docs.component.ts
@@ -1,9 +1,8 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {SharedModule} from "../../theme/shared/shared.module";
-import SwaggerUI,  {SwaggerUIOptions } from 'swagger-ui';
-import SwaggerUIStandalonePreset from 'swagger-ui';
-// import {SwaggerConfigs, SwaggerUIBundle, SwaggerUIStandalonePreset} from 'swagger-ui-dist';
+// import SwaggerUI,  {SwaggerUIOptions } from 'swagger-ui';
+import { SwaggerConfigs, SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
 import {environment} from "../../../environments/environment";
 @Component({
   selector: 'app-api-docs',
@@ -13,22 +12,22 @@ import {environment} from "../../../environments/environment";
   styleUrls: ['./api-docs.component.scss']
 })
 export default class ApiDocsComponent implements  AfterViewInit {
-    // @ts-ignore
+    // @ts-expect-error swaggerDom not initialized
     @ViewChild('swagger') swaggerDom: ElementRef<HTMLDivElement>
     private  apiUrl = environment.gcpCommAccApiUrl;
     title: string = 'Hello';
     constructor() {}
 
     ngAfterViewInit() {
-        const swagConfig: SwaggerUIOptions = {
+        const swagConfig: SwaggerConfigs = {
             url: `${this.apiUrl}api-docs-json`,
             requestInterceptor: (request: any) => {
                 console.log('request:'+JSON.stringify(request));
-                let addAuth = (request['url'].indexOf('/login') == -1);
+                const addAuth = (request['url'].indexOf('/login') === -1);
                 const user = sessionStorage.getItem('user');
                 console.log('requestInterceptor - user: ', user);
                 if(user) {
-                    let currentUser = JSON.parse(user);
+                    const currentUser = JSON.parse(user);
                     if (currentUser && currentUser.idToken && addAuth) {
                         request['headers']['Authorization'] = 'Bearer ' + currentUser.idToken;
                     } else {
@@ -46,9 +45,11 @@ export default class ApiDocsComponent implements  AfterViewInit {
             dom_id: 'swagger',
             domNode: this.swaggerDom.nativeElement,
             deepLinking: true,
-
+            presets: [SwaggerUIBundle['presets'].apis, SwaggerUIStandalonePreset],
+            layout: 'StandaloneLayout'
         };
         console.log('ApiDocsComponent - ngAfterViewInit - swagConfig: ', JSON.stringify(swagConfig));
-        SwaggerUI(swagConfig);
+        // SwaggerUI(swagConfig);
+        SwaggerUIBundle(swagConfig)
     }
 }
