61433f22ce09f0284502d497a408abd7cff4d27e
ipv6wiz <john@thejohnpage.com>
2024-07-11 17:22:45 -0700

    Deployed Version 0.0.132. Updated Dashboard theme

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -2,8 +2,8 @@ export class GradientConfig {
   static layout = 'vertical'; // vertical, horizontal
   static subLayout = ''; // horizontal-2
   static isCollapse_menu = false; // true
-  static layoutType = 'menu-light'; // menu-dark, menu-light, dark
-  static header_back_color = 'header-blue'; // header-blue, header-red, header-purple, header-info, header-dark, header-orange
+  static layoutType = 'menu-dark'; // menu-dark, menu-light, dark
+  static header_back_color = 'header-yellow'; // header-blue, header-red, header-purple, header-info, header-dark, header-orange
   static isRtlLayout = false; // true
   static nav_fixed_layout = true; // false
   static header_fixed_layout = true; // false
