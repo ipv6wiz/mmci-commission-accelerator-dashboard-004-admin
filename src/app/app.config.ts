758b2a816c7c8bca2b7778c0447a776bfe7cdd37
ipv6wiz <john@thejohnpage.com>
2024-07-19 17:56:37 -0700

    Deployed Version 0.0.190. Adding Kanban - Phase 1 - bug fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,3 @@
 export class AppConfig {
-  version: string = '0.0.189';
+  version: string = '0.0.190';
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { registerLicense } from '@syncfusion/ej2-base';
 
 if (environment.production) {
   enableProdMode();
 }
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF5cWWJCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXheeXZWR2heVU1+XEU=');
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
