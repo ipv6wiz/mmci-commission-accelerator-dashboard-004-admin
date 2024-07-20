eddd1e9db1bfe85d3537d09c9994e8e6273dbc1f
ipv6wiz <john@thejohnpage.com>
2024-07-19 18:35:45 -0700

    Deployed Version 0.0.194. Adding Kanban - Phase 1 - bug fixes - license issue

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { registerLicense } from '@syncfusion/ej2-base';
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXhfeXZUR2ZZWEJxX0c=');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXhfeXZURGhYVEF1XEE=');
 
 if (environment.production) {
   enableProdMode();
